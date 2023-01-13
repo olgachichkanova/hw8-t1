bed8fed0749778d699072b7d55014323249285fc
Olga Andriychenko <olga@glasnostic.com>
2023-01-13 10:27:12 +0800

    added solutuin without custom hook

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,10 +1,32 @@
+import { useState, useEffect } from "react";
 import useFetchData from '../hooks/useFetchData'
 import Loader from "../Loader/Loader"
 import "./Details.css"
 
 const Details = ({info}) => {
-    
-    const [{data, isLoading, hasError}] = useFetchData(`${process.env.REACT_APP_BASE_URL}/${info.id}.json`, info)
+    // const [{data, isLoading, hasError}] = useFetchData(`${process.env.REACT_APP_BASE_URL}/${info.id}.json`, info)
+    const [data, setData] = useState([])
+    const [isLoading, setLoading] = useState(false)
+    const [hasError, setError] = useState(null)
+
+    const url = `${process.env.REACT_APP_BASE_URL}/${info.id}.json`;
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                if(!response.ok){
+                    throw new Error(response.statusText)
+                }
+                const data = await response.json()
+                setData(data)
+                setError(null)
+            } catch (error) {
+                setError(error)
+            } finally {setLoading(false)}
+        }
+        fetchData()
+    }, [url, info.id])
     
     return (
         <div className="details__box">
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,30 @@
+import { useState, useEffect } from "react";
 import useFetchData from '../hooks/useFetchData'
 import Loader from '../Loader/Loader';
 import "./List.css"
 
 const List = ({setInfo}) => {
-    const [{data, isLoading, hasError}] = useFetchData(`${process.env.REACT_APP_BASE_URL}/users.json`)
+    // const [{data, isLoading, hasError}] = useFetchData(`${process.env.REACT_APP_BASE_URL}/users.json`)
+    const url = `${process.env.REACT_APP_BASE_URL}/users.json`
+    const [list, setList] = useState([])
+    const [isLoading, setLoading] = useState(false)
+    const [hasError, setError] = useState(null)
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                if(!response.ok){
+                    throw new Error(response.statusText)
+                }
+                const data = await response.json()
+                setList(data)
+                setError(null)
+            } catch (error) {
+                setError(error)
+            } finally {setLoading(false)}
+        }
+        fetchData()
+    }, [url])
     const handleSelect = (item) => {
         setInfo(item)
     }
@@ -13,7 +34,7 @@ const List = ({setInfo}) => {
             {hasError && <p>{hasError}</p>}
             {!isLoading &&
                 <ul>
-                    {data.map(item => <li key={item.id}><button onClick={() => handleSelect(item)}>{item.name}</button></li>)}
+                    {list.map(item => <li key={item.id}><button onClick={() => handleSelect(item)}>{item.name}</button></li>)}
                 </ul>
             }
         </div>
